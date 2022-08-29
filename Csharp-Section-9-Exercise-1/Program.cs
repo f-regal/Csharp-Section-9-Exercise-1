3a7563f7977bc70b88f3388694e805433cff388e
Faysal Regal <faysalregal@gmail.com>
2022-08-29 18:26:14 +0100

    first commit

---
+++ Csharp-Section-9-Exercise-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpSection9Exercise1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
