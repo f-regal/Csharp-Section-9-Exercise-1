60e4661d414b53ba57721b0824ba81805197c006
Faysal Regal <faysalregal@gmail.com>
2022-08-29 18:43:08 +0100

    almost completed exercise 1

--- Csharp-Section-9-Exercise-1/Program.cs
+++ Csharp-Section-9-Exercise-1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace CsharpSection9Exercise1
 {
@@ -6,7 +8,18 @@ namespace CsharpSection9Exercise1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Exercise 1- Write a program that reads a text file and displays the number of words.
+
+            var path = @"/Users/faysalregal/Desktop/C# Fundamentals/Section 9 Working with Files/Exercises/Exercise 1/Exercise-1-Text-File/Exercise-1";
+
+            // Calling the ReadAllText(String, Encoding)
+            // function
+            string readText = File.ReadAllText(path);
+
+            // Printing the file contents
+            Console.WriteLine(readText.Length);
+
+            //remember to split the sentence and count only the letters
         }
     }
 }
