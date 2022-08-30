1a80990a8c59257665e8f0fe558e358fe116f68d
Faysal Regal <faysalregal@gmail.com>
2022-08-30 20:32:39 +0100

    Completed exercise 1 and 2 section 9

--- Csharp-Section-9-Exercise-1/Program.cs
+++ Csharp-Section-9-Exercise-1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -15,12 +17,29 @@ namespace CsharpSection9Exercise1
 
             // Calling the ReadAllText(String, Encoding) function
             string readText = File.ReadAllText(path);
-            readText = Regex.Replace(readText, @"\s", "");
-            char[] userInput = readText.ToCharArray();
+            var words = readText.Split(' ');
 
             // Printing the file contents
-            Console.WriteLine(userInput.Length);
+            Console.WriteLine("Number of words: " + words.Length);
 
+
+
+
+            //Exercise 2 - Write a program that reads a text file and displays the longest word in the file.
+
+            List<int> lengthOfWords = new List<int>();
+            var largestWord = words[0];
+      
+            for (int i = 0; i < words.Length; i++)
+            {
+                lengthOfWords.Add(words[i].Length);
+                if (words[i].Length > largestWord.Length)
+                {
+                    largestWord = words[i];
+                }
+            }
+
+            Console.WriteLine(largestWord);
         }
     }
 }
